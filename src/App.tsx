88a5e17bf5f86740080ab1d4312ed629d1f8d6cd
Mustapha <tahirumustapha13@gmail.com>
2024-10-02 18:43:05 +0000

    Initialized project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+function App() {
+  return (
+    <main className=" h-[100vh] flex items-center justify-center">
+      <h1>Hello</h1>
+    </main>
+  );
+}
+
+export default App;
