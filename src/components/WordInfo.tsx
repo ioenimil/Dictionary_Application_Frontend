7ba4877c4a0982b24b377f1e4709996eaf0f66a4
Mustapha <tahirumustapha13@gmail.com>
2024-10-09 10:29:31 +0000

    Did refactoring in the search component

--- src/components/WordInfo.tsx
+++ src/components/WordInfo.tsx
@@ -1,17 +1,13 @@
 import { WordResult } from "@/types";
 import PartOfSpeech from "./PartOfSpeech";
-import Pronounce from "./Pronounce";
-
 
 interface Props {
   results: WordResult[];
- 
 }
-const WordInfo: React.FC<Props> = ({ results}) => {
+const WordInfo: React.FC<Props> = ({ results }) => {
   return (
     <div className=" mt-8  max-h-[87%] overflow-y-auto ">
-      <Pronounce results={results} />
-      <PartOfSpeech   results={results} />
+      <PartOfSpeech results={results} />
     </div>
   );
 };
