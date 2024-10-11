1ca4e5369eb812d484845696ed4d43f30155175e
Mustapha <tahirumustapha13@gmail.com>
2024-10-11 13:55:50 +0000

    refactor: removed console.log statement

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -6,7 +6,7 @@ interface Props {
 const ErrorPage: React.FC<Props> = ({ wordNotFoundError }) => {
   return (
     <div className=" w-full md:w-[736px]   md:h-[204px] flex flex-col items-center justify-center ">
-        <p className=" mb-3 mt-5 text-center ">:confused:</p>
+        <p className=" mb-3 mt-5 text-center ">😉</p>
       <h2 className=" font-bold text-sm text-[#2D2D2D] dark:text-white">
         {wordNotFoundError.title}
       </h2>
--- src/components/WordInfo.tsx
+++ src/components/WordInfo.tsx
@@ -1,15 +1,18 @@
-
 import { WordResult } from "types";
 import PartOfSpeech from "./PartOfSpeech";
+import PronounceWord from "./PronouceWord";
 
 interface Props {
   results: WordResult[];
 }
+
 const WordInfo: React.FC<Props> = ({ results }) => {
   return (
-    <div className="mt-6 max-h-[87%] overflow-y-auto scrollbar-thin">
+    <div className="mt-6 bg-red-600 ">
+      <PronounceWord results={results} />
       <PartOfSpeech results={results} />
     </div>
   );
 };
+
 export default WordInfo;
