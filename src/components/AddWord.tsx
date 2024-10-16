246f6d3af5b91b40a898f0974b5f8f3200d28856
MamaFati <abufati876@gmail.com>
2024-10-16 07:31:07 +0000

    perf: improve add word inputs

--- src/components/AddWord.tsx
+++ src/components/AddWord.tsx
@@ -13,6 +13,7 @@ interface AddWordModalProps {
 
 const AddWordModal: React.FC<AddWordModalProps> = ({ onClose, onAddWord }) => {
   const [word, setWord] = useState("");
+  const [scource,setScource] = useState("");
   const [partOfSpeech, setPartOfSpeech] = useState("");
   const [synonyms, setSynonyms] = useState("");
   const [link, setLink] = useState("");
@@ -44,49 +45,72 @@ const AddWordModal: React.FC<AddWordModalProps> = ({ onClose, onAddWord }) => {
             onClick={handleAdd}
             className="flex h-[33px] w-[119px] gap-1 rounded bg-global_blue px-4 py-1 text-white"
           >
-            <img src={addIcon} alt="addIcon" className="mt-[6px] h-3 w-3" />
+            <img src={addIcon} alt="addIcon" className="mt-[6px] h-3 w-3" /> 
+
             <span className="text-center text-base font-medium">Add</span>
           </button>
         </nav>
-        <h2 className="mb-4 text-xl font-bold">Add New Word</h2>
-        <div className="flex h-[89px] w-[1092px] gap-2">
-          <input
-            type="text"
-            value={word}
-            onChange={(e) => setWord(e.target.value)}
-            placeholder="Word to add"
-            className="mb-2 h-[89px] w-[540px] rounded-xl border focus:border-global_blue focus:outline-none"
-          />
-          <input
-            type="text"
-            value={word}
-            onChange={(e) => setWord(e.target.value)}
-            placeholder="Scource"
-            className="h-[89px] w-[540px] rounded-xl border focus:border-global_blue focus:outline-none"
-          />
-        </div>
-        <input
-          type="text"
-          value={partOfSpeech}
-          onChange={(e) => setPartOfSpeech(e.target.value)}
-          placeholder="Pronunciation"
-          className="my-5 h-[89px] w-[1092px] rounded-xl border focus:border-global_blue focus:outline-none"
-        />
+        <span className="mb-4 text-xl font-bold"> word List / Add Word </span><br />
+        <span>Fields marked with (*) are required.</span>
 
-        <input
-          type="text"
-          value={synonyms}
-          onChange={(e) => setSynonyms(e.target.value)}
-          placeholder="noun"
-          className="mb-5 h-[358px] w-[1092px] rounded-xl border focus:border-global_blue focus:outline-none"
-        />
-        <input
-          type="text"
-          value={link}
-          onChange={(e) => setLink(e.target.value)}
-          placeholder="+ Add part of speech"
-          className="mb-4 h-[89px] w-[1092px] rounded-xl border-2 border-dashed border-black bg-grayBg p-2 outline-none placeholder:text-center placeholder:text-black"
-        />
+        <form action="">
+            <div className="flex h-[89px] w-[1092px] gap-2">
+              <div className=" mb-2 h-[89px] w-[540px] bg-white rounded-xl pl-5 border hover:border-global_blue  ">
+                <label
+                  className= "  "
+                >
+                  Word to add
+                </label>
+                <input
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  
+                  className="bg-transparent w-full  outline-none font-medium  mt-3"
+                />
+                
+              </div>
+              <div className="mb-2 h-[89px] w-[540px] bg-white rounded-xl pl-5 border hover:border-global_blue">
+                 <label
+                  className= "  "
+                >
+                  Scource
+                </label>
+                <input
+                type="text"
+                value={scource}
+                onChange={(e) => setScource(e.target.value)}
+                 
+                className="bg-transparent w-full  outline-none font-medium  mt-3 "
+                />
+              </div>
+           
+            </div>
+          <div className="h-[89px] w-[1092px] bg-white rounded-xl pl-5 border hover:border-global_blue">
+              <input
+              type="text"
+              value={partOfSpeech}
+              onChange={(e) => setPartOfSpeech(e.target.value)}
+              placeholder="Pronunciation"
+              className=" bg-transparent w-full  outline-none font-medium  mt-3"
+              />
+          </div>
+
+          <input
+            type="text"
+            value={synonyms}
+            onChange={(e) => setSynonyms(e.target.value)}
+            placeholder="noun"
+            className="mb-5 h-[358px] w-[1092px] rounded-xl border focus:border-global_blue focus:outline-none"
+          />
+          <input
+            type="text"
+            value={link}
+            onChange={(e) => setLink(e.target.value)}
+            placeholder="+ Add part of speech"
+            className="mb-4 h-[89px] w-[1092px] rounded-xl border-2 border-dashed border-black bg-grayBg p-2 outline-none placeholder:text-center placeholder:text-black"
+          />
+        </form>
       </div>
     </main>
   );
