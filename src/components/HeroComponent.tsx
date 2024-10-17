486cc3b08c82c411f0b3b651900b7eb690a26950
IT Compliance <compliance@amalitech.org>
2024-10-17 10:29:22 +0000

    refactor: resolve conflict

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -1,16 +1,12 @@
 import NavBar from "./NavBar";
- 
-import Search from "./SearchComponent";
-import WordList from "./WordList";
+
+import SearchContent from "./SearchContent";
 const HeroComponent = () => {
   return (
     <div className="m-auto h-full w-[329px] md:w-[689px] lg:w-[739px]">
       <NavBar />
- 
-      <Search />
-      <WordList />
+
       <SearchContent />
- 
     </div>
   );
 };
