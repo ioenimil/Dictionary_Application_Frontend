43103973c5acdda0078482b07053e49f17352a47
IT Compliance <compliance@amalitech.org>
2024-10-17 09:50:11 +0000

    refactor: remove repeated codes

--- src/components/wordListTable.tsx
+++ src/components/wordListTable.tsx
@@ -27,12 +27,10 @@ const WordListTable: React.FC = () => {
 
   useEffect(() => {
     const sampleWords: Word[] = [];
-
     setWords(sampleWords);
   }, []);
 
   const handleEdit = () => {};
-
   const handleDelete = (index: number) => {
     setWords(words.filter((_, i) => i !== index));
   };
@@ -44,11 +42,11 @@ const WordListTable: React.FC = () => {
     }));
   };
 
-  const sortedWords = [...words].sort((a, b) => {
-    return sortOrder === "asc"
+  const sortedWords = [...words].sort((a, b) =>
+    sortOrder === "asc"
       ? a.word.localeCompare(b.word)
-      : b.word.localeCompare(a.word);
-  });
+      : b.word.localeCompare(a.word),
+  );
 
   const filteredWords = sortedWords.filter(
     (word) =>
@@ -61,21 +59,64 @@ const WordListTable: React.FC = () => {
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = filteredWords.slice(indexOfFirstItem, indexOfLastItem);
 
-  const handlePageChange = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-  };
+  const handlePageChange = (pageNumber: number) => setCurrentPage(pageNumber);
+  const handleNextPage = () =>
+    currentPage < totalPages && setCurrentPage((prev) => prev + 1);
+  const handlePreviousPage = () =>
+    currentPage > 1 && setCurrentPage((prev) => prev - 1);
 
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage((prevPage) => prevPage + 1);
-    }
-  };
+  const renderDropdown = (index: number) => (
+    <div className="absolute right-0 mt-2 w-24 rounded-md bg-white shadow-lg">
+      <button
+        onClick={handleEdit}
+        className="block w-full px-4 py-2 text-left text-sm text-textGrey hover:bg-gray-100"
+      >
+        Edit
+      </button>
+      <button
+        onClick={() => handleDelete(index)}
+        className="block w-full px-4 py-2 text-left text-sm text-red-600 hover:bg-red-100"
+      >
+        Delete
+      </button>
+    </div>
+  );
 
-  const handlePreviousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage((prevPage) => prevPage - 1);
-    }
-  };
+  const renderPagination = () => (
+    <div className="ml-[403px] mt-4 flex h-[50px] w-[395px] items-center justify-center gap-[10px] rounded-3xl bg-white">
+      <button
+        onClick={handlePreviousPage}
+        disabled={currentPage === 1}
+        className={`rounded-full px-2 py-1 ${currentPage === 1 ? "text-textGrey" : "text-black"}`}
+      >
+        <FiChevronLeft size={24} />
+      </button>
+      {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNumber) => (
+        <button
+          key={pageNumber}
+          onClick={() => handlePageChange(pageNumber)}
+          className={`h-8 w-8 rounded-full px-3 py-1 transition-colors duration-200 ${
+            pageNumber === currentPage
+              ? "bg-blueBg text-white"
+              : "bg-gray-200 text-white hover:bg-blue-200"
+          }`}
+        >
+          {pageNumber}
+        </button>
+      ))}
+      <button
+        onClick={handleNextPage}
+        disabled={currentPage === totalPages}
+        className={`rounded-full px-11 py-1 ${currentPage === totalPages ? "text-textGrey" : "text-black"}`}
+      >
+        <FiChevronRight size={20} />
+      </button>
+    </div>
+  );
+
+  const tableHeaderClasses =
+    "px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-black dark:text-textGrey";
+  const tableCellClasses = "whitespace-nowrap px-6 py-4 text-sm text-textGrey";
 
   return (
     <>
@@ -87,37 +128,24 @@ const WordListTable: React.FC = () => {
         onFilter={(filter) => setFilterTerm(filter)}
         onSort={() => setSortOrder(sortOrder === "asc" ? "desc" : "asc")}
       />
+
       <table className="mt-2 w-[1200px] divide-y divide-gray-500">
         <thead className="h-[75px] bg-white dark:bg-[#404040] dark:text-white">
           <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-black dark:text-textGrey">
-              Word
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-black dark:text-textGrey">
-              Part of Speech
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-black dark:text-textGrey">
-              Synonyms
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-black dark:text-textGrey">
-              Link
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-black dark:text-textGrey"></th>
+            <th className={tableHeaderClasses}>Word</th>
+            <th className={tableHeaderClasses}>Part of Speech</th>
+            <th className={tableHeaderClasses}>Synonyms</th>
+            <th className={tableHeaderClasses}>Link</th>
+            <th className={tableHeaderClasses}></th>
           </tr>
         </thead>
         <tbody className="h-[75px] divide-y divide-gray-200 bg-white dark:bg-[#404040]">
           {currentItems.map((word, index) => (
             <tr key={index} className="border-b border-gray-200">
-              <td className="whitespace-nowrap px-6 py-4 text-sm text-textGrey">
-                {word.word}
-              </td>
-              <td className="whitespace-nowrap px-6 py-4 text-sm text-textGrey">
-                {word.partOfSpeech}
-              </td>
-              <td className="whitespace-nowrap px-6 py-4 text-sm text-textGrey">
-                {word.synonyms.join(" ")}
-              </td>
-              <td className="whitespace-nowrap px-6 py-4 text-sm text-textGrey underline">
+              <td className={tableCellClasses}>{word.word}</td>
+              <td className={tableCellClasses}>{word.partOfSpeech}</td>
+              <td className={tableCellClasses}>{word.synonyms.join(" ")}</td>
+              <td className={`${tableCellClasses} underline`}>
                 <a href={word.link} target="_blank" rel="noopener noreferrer">
                   {word.link}
                 </a>
@@ -126,66 +154,14 @@ const WordListTable: React.FC = () => {
                 <button onClick={() => toggleDropdown(index)}>
                   <FiMoreHorizontal className="text-textGrey" />
                 </button>
-
-                {dropdownVisible[index] && (
-                  <div className="absolute right-0 mt-2 w-24 rounded-md bg-white shadow-lg">
-                    <button
-                      onClick={() => handleEdit()}
-                      className="block w-full px-4 py-2 text-left text-sm text-textGrey hover:bg-gray-100"
-                    >
-                      Edit
-                    </button>
-                    <button
-                      onClick={() => handleDelete(index)}
-                      className="block w-full px-4 py-2 text-left text-sm text-red-600 hover:bg-red-100"
-                    >
-                      Delete
-                    </button>
-                  </div>
-                )}
+                {dropdownVisible[index] && renderDropdown(index)}
               </td>
             </tr>
           ))}
         </tbody>
       </table>
 
-      <div className="ml-[403px] mt-4 flex h-[50px] w-[395px] items-center justify-center gap-[10px] rounded-3xl bg-white">
-        <button
-          onClick={handlePreviousPage}
-          disabled={currentPage === 1}
-          className={`rounded-full px-2 py-1 ${
-            currentPage === 1 ? "text-textGrey" : "text-black"
-          }`}
-        >
-          <FiChevronLeft size={24} />
-        </button>
-
-        {Array.from({ length: totalPages }, (_, i) => i + 1).map(
-          (pageNumber) => (
-            <button
-              key={pageNumber}
-              onClick={() => handlePageChange(pageNumber)}
-              className={`h-8 w-8 rounded-full px-3 py-1 transition-colors duration-200 ${
-                pageNumber === currentPage
-                  ? "bg-blueBg text-white"
-                  : "bg-gray-200 text-white hover:bg-blue-200"
-              }`}
-            >
-              {pageNumber}
-            </button>
-          ),
-        )}
-
-        <button
-          onClick={handleNextPage}
-          disabled={currentPage === totalPages}
-          className={`rounded-full px-11 py-1 ${
-            currentPage === totalPages ? "text-textGrey" : "text-black"
-          }`}
-        >
-          <FiChevronRight size={20} />
-        </button>
-      </div>
+      {renderPagination()}
     </>
   );
 };
